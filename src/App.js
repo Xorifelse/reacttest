1453dcd8c2bea8568597b0d18ebb09217884c1ed
Xorifelse <johan.kruse.com>
2018-08-20 12:33:48 +0200

    added Lightswitch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import Title from './components/Title'
 import logo from './logo.svg';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
@@ -10,9 +13,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <main>
+          <Title content="Hello" />
+          <LightSwitch />
+        </main>
       </div>
     );
   }
